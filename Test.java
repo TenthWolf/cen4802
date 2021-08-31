47a76616e8271269e6460a48006433df08dc3663
TenthWolf <64280904+TenthWolf@users.noreply.github.com>
2021-08-31 13:51:24 -0400

    TestChanges
    Added comments and JavaDoc implementation explaining code use step by step

--- Test.java
+++ Test.java
@@ -1,11 +1,29 @@
+/*
+ * User: Rodolfo Puig
+ * Assignment 2: Fibonacci Sequence
+ * Date: August 31, 2021
+ */
 
+/**
+ * 
+ * @author Rodolfo Puig
+ *
+ */
 public class Test {
+
+	/**
+	 * 
+	 * @param n - will be used to generate fibonacci results using recursion
+	 * @return - retrieve the recursive data
+	 */
 
 	public static int fib(int n) {
 
+		// default must be greater than 1
 		if (n <= 1)
 			return n;
 		else
+			// fibonacci sequence
 			return fib(n - 1) + fib(n - 2);
 
 	}
@@ -13,7 +31,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// default number
 		int n = 10;
+		// n cannot be less than 0
 		if (n < 0)
 			System.out.println("Fibonacci number not recognized");
 		else
